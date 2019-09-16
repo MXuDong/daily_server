f068461e7a337ddafefee075f4ff8565ccce8a49
MXuDong <2941884109@qq.com>
2019-09-16 19:47:29 +0800

    add doc

--- src/main/java/io/github/com/daily_server/DailyServerApplication.java
+++ src/main/java/io/github/com/daily_server/DailyServerApplication.java
@@ -3,6 +3,11 @@ package io.github.com.daily_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * the project start class
+ *
+ * @author Dong
+ */
 @SpringBootApplication
 public class DailyServerApplication {
 

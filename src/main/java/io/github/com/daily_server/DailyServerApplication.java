e407e17e34158953275b180bc89e306aaa85431f
MXuDong <2941884109@qq.com>
2019-09-16 19:45:44 +0800

    init codes

---
+++ src/main/java/io/github/com/daily_server/DailyServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.com.daily_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyServerApplication.class, args);
+    }
+
+}
